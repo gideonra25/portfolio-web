cdbd59e3ad8b7970cf12f47d3c9dc268bd4b7eca
Gideon Ramot Aprilliano <89437021+gideonra25@users.noreply.github.com>
2023-12-16 05:16:01 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
       pageTransition: {name: 'page', mode: 'out-in'},
       head: {
           meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+          script: [{src: 'https://kit.fontawesome.com/9f47c46af7.js', async: true, crossorigin: 'anonymous'},]
       }
   },
 
